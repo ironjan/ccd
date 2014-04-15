061a4a26629d2570453b93e4399368b225ff6633
Jan Lippert <ljan@mail.upb.de>
2014-04-15 17:53:29 +0200

    Added gradle files, updated app logo

--- drawable-svg-sources/create_all_sizes.sh
+++ drawable-svg-sources/create_all_sizes.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+RESDIR=../src/main/res
 for i in *png;do 
- convert $i -resize 36x36 ../res/drawable-ldpi/$i; 
- convert $i -resize 48x48 ../res/drawable-mdpi/$i; 
- convert $i -resize 72x72 ../res/drawable-hdpi/$i; 
- convert $i -resize 96x96 ../res/drawable-xhdpi/$i; 
+ convert $i -resize 36x36 $RESDIR/drawable-ldpi/$i; 
+ convert $i -resize 48x48 $RESDIR/drawable-mdpi/$i; 
+ convert $i -resize 72x72 $RESDIR/drawable-hdpi/$i; 
+ convert $i -resize 96x96 $RESDIR/drawable-xhdpi/$i; 
 done
